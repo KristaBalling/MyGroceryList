76e93156709d6935dcd19125f1dff04762a60c21
Krista Blachian <krista.blachian@scifusion360.com>
2019-03-13 09:34:07 -0400

    gets instance of groceries

--- app/src/main/java/Data/DatabaseHandler.java
+++ app/src/main/java/Data/DatabaseHandler.java
@@ -1,10 +1,12 @@
 package Data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -44,10 +46,45 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     //Add Grocery
     public void AddGrocery(Grocery grocery) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Constants.KEY_GROCERY_ITEM, grocery.getName());
+        values.put(Constants.KEY_QTY_NUMBER, grocery.getQuantity());
+        values.put(Constants.KEY_DATE_NAME, java.lang.System.currentTimeMillis());
+
+        //Insert the row
+        db.insert(Constants.TABLE_NAME, null, values);
+
+        Log.d("Saved!!", "Saved to DB");
     }
 
     //Get a Grocery
     private Grocery getGrocery(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor cursor = db.query(Constants.TABLE_NAME, new String[] {Constants.KEY_ID,
+        Constants.KEY_GROCERY_ITEM, Constants.KEY_QTY_NUMBER, Constants.KEY_DATE_NAME},
+                Constants.KEY_ID + "=?",
+                new String[] {String.valueOf(id)}, null, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+
+            Grocery grocery = new Grocery();
+            grocery.setId(Integer.parseInt(cursor.getString(cursor.getColumnIndex(Constants.KEY_ID))));
+            grocery.setName(cursor.getString(cursor.getColumnIndex(Constants.KEY_GROCERY_ITEM)));
+            grocery.setQuantity(cursor.getString(cursor.getColumnIndex(Constants.KEY_QTY_NUMBER)));
+
+            //convert timestamp to something readable
+            java.text.DateFormat dateFormat = java.text.DateFormat.getDateInstance();
+            String formatedDate = dateFormat.format(new Date(cursor.getLong(cursor.getColumnIndex(Constants.KEY_DATE_NAME)))
+            .getTime());
+
+            grocery.setDateItemAdded(formatedDate);
+
+            return grocery;
+        }
 
         return null;
     }
